8a99f47a26b8203b1516bea0d3d052f482d33f29
Claux18 <clauxius18@hotmail.com>
2026-02-25 11:39:12 -0600

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,34 @@
+var nombre = "Claudia";
+console.log(nombre);
+
+let apellido = "Vazquez";
+console.log(apellido);
+
+const edad = 25;
+console.log(edad);
+
+let carrera = "Ingenieria en Computacion";
+console.log(carrera);
+
+let correo = "clauxius18@hotmail.com"
+console.log(correo);
+
+var numero1 = 10;
+var numero2 = 10;  
+var res = numero1 - numero2;
+console.log(res);
+
+var numero3 = 10;
+var numero4 = 10;
+var suma = numero3 + numero4;
+console.log(suma);
+
+var numero5 = 10;
+var numero6 = 10;
+var multp = numero5 * numero6;
+console.log(multp);
+
+var numero7 = 10;
+var numero8 = 10;
+var division = numero7 / numero8;
+console.log(division);
