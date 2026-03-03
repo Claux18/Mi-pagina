aabdad818ed56a4dd09e5bb9902a8258a818a1d1
Claux18 <clauxius18@hotmail.com>
2026-03-03 15:19:35 -0600

    Add files via upload

---
+++ app8.js
@@ -0,0 +1,9 @@
+let mensaje = prompt("Ingresa tu pass");
+if(mensaje == "123456"){
+    console.log("Genial :)");
+
+}else{
+    console.log("Error pass mal :(");
+
+}
+console.log("Fin del programa ...");
